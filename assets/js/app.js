6380cda0e2ba19bbfbd4b8baa94a101825f1760d
unknown <justinmandac@gmail.com>
2015-05-21 23:57:09 +0800

    init

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function() {
+    
+    Reveal.initialize({
+      center: true,
+      controls: false,
+      mouseWheel: true,
+      overview: false,
+      transition: 'slide',
+      parallaxBackgroundImage: 'assets/images/hor.jpg',
+      parallaxBackgroundSize: '2483px 1357px'
+    });
+    
+  })
+})(jQuery);
