7a5414b474d9d011ae1e3a1fae34f86d61c648be
unknown <justinmandac@gmail.com>
2015-05-24 18:18:53 +0800

    overhaul

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,38 @@
 (function($){
   $(function() {
+    var indicators = $('.indicator');
+    var scroll_trigger = $('#scroll-trigger');
     
-    Reveal.initialize({
-      center: true,
-      controls: false,
-      mouseWheel: true,
-      overview: false,
-      transition: 'slide',
-      parallaxBackgroundImage: 'assets/images/hor.jpg',
-      parallaxBackgroundSize: '2483px 1357px'
+    
+    scroll_trigger.on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 0
+      }, 700, 'swing');
+    });
+    
+    indicators.click(function () {
+      var parent = $(this).parents('.timeline-item');
+      var content = parent.children('.item-content');
+      console.log(parent);
+      console.log(content);
+      
+      if(content.hasClass('hidden')) {
+        content.removeClass('hidden');
+        
+        $('html, body').stop().animate({
+          'scrollTop': content.offset().top - 0
+        }, 700, 'swing');        
+        
+      }
+      else {
+        content.addClass('hidden');
+      }
+    
     });
     
   })
