7984b1c61c849ad901503748d083e8d007e5d319
unknown <justinmandac@gmail.com>
2015-06-20 15:07:13 +0800

    overhaul

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,66 +1,11 @@
 (function($){
   $(function() {
-    var indicators = $('.indicator');
-    var scroll_trigger = $('#scroll-trigger');
-    
-    
-    scroll_trigger.on('click',function (e) {
-      e.preventDefault();
 
-      var target = this.hash;
-      var $target = $(target);
-
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top - 0
-      }, 700, 'swing');
-    });
-    
-    indicators.click(function () {
-      var parent = $(this).parents('.timeline-item');
-      var content = parent.children('.item-content');
-      console.log(parent);
-      console.log(content);
-      
-      if(content.hasClass('hidden')) {
-        content.removeClass('hidden');
-        
-        $('html, body').stop().animate({
-          'scrollTop': content.offset().top - 0
-        }, 700, 'swing');        
-        
-      }
-      else {
-        content.addClass('hidden');
-      }
-    
-    });
     
     //mouse tracker
     var max_height = document.documentElement.clientHeight;
     var max_width  = document.documentElement.clientWidth;
     var container = document.getElementById('home-section');
-
-/*    console.log('Doc size is (width x height) : ' + max_width + ',' + max_height);
-
-    container.onmousemove = function(e) {
-      var x_gain = 2;
-      var y_gain = 4;   
-
-      var style = window.getComputedStyle(container);
-      var bg_x = style.getPropertyValue('background-position-x').split('%')[0];
-      var bg_y = style.getPropertyValue('background-position-y').split('%')[0];
-
-      console.log(bg_x);
-
-      var x =   ((e.clientX+max_width/2) - max_width)/max_width;
-      var y =   ((e.clientY+max_height/2) - max_height)/max_height;
-
-      var x_mov = x_gain * x;
-      var y_mov = y_gain * y;
-
-      container.style.backgroundPositionX = (50+x_mov).toString()+'%';
-      container.style.backgroundPositionY = (50+y_mov).toString()+'%';    
-    };*/
 
     document.onscroll = function () {
       var y_offset = window.scrollY;
